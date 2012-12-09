1cf7508c596905bffe4db6d31ac7a7af665c13f0
mrcrow <wwz.michael@gmail.com>
2012-12-09 22:53:22 +0800

    Initial Commit

---
+++ LogBook/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  LogBook
+//
+//  Created by Wu Wenzhi on 12-12-9.
+//  Copyright (c) 2012年 Wu Wenzhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@property (strong, nonatomic) UISplitViewController *splitViewController;
+
+@end
---
+++ LogBook/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LogBook
+//
+//  Created by Wu Wenzhi on 12-12-9.
+//  Copyright (c) 2012年 Wu Wenzhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
